1cfd54f4274c879239ecdc0afc3944a0744cf57c
Magnus Nytun <Magnus@Magnuss-MacBook-Air-4.local>
2022-03-20 11:56:45 +0100

    work in progress: need to add functionality for when you init with crossover vecs

--- src/generation.py
+++ src/generation.py
@@ -1,41 +1,58 @@
 import random
-import numpy as np
+from src.individual import Individual
 
 
 class Generation:
-    def __init__(self, pop_size, vec_len, score_func, crossover_vecs = []):
+    def __init__(self, pop_size, n_genes, score_func, crossover_vecs = None):
         self.pop_size = pop_size
-        self.vec_len = vec_len
+        self.n_genes = n_genes
         self.score_func = score_func
         self.pop = []
-        self.crossover_vecs = crossover_vecs
-        self._init_pop()
+
+        if not crossover_vecs:
+            self._init_pop()
+        
+        else:
+            self.pop = crossover_vecs
 
 
     def _init_pop(self):
-        """
-        Lager en ny populasjon med individer ved en ny generasjon. Tar hensyn til crossover individer.
+        for i in range(self.pop_size):
+            vec = [random.randint(0, 1) for _ in range(self.n_genes)]
+            fitness = self.score_func(vec)
+            
+            indi = Individual(i, vec, fitness)
+            self.pop.append(indi)
 
-        :return:
-        """
-        if len(self.crossover_vecs) > 0:
-            for idx, ind in enumerate(self.crossover_vecs):
-                self.pop.append(
-                    {
-                        "idx" : idx,
-                        "vec": ind["vec"],
-                        "fitness": ind["fitness"],
-                     }
-                )
-
-        for i in range(len(self.crossover_vecs), self.pop_size):
-            vec = [random.randint(0, 1) for _ in range(self.vec_len)]
-
-            self.pop.append({
-                "idx": i,
-                "vec" : vec,
-                "fitness" : self.score_func(vec)
-            })
+    def _sort_generation(self):
+        self.pop.sort(key = lambda x: x.fitness)
 
     def mutate(self):
         raise NotImplemented
+
+
+
+    # def _init_pop(self):
+    #     """
+    #     Lager en ny populasjon med individer ved en ny generasjon. Tar hensyn til crossover individer.
+
+    #     :return:
+    #     """
+    #     if len(self.crossover_vecs) > 0:
+    #         for idx, ind in enumerate(self.crossover_vecs):
+    #             self.pop.append(
+    #                 {
+    #                     "idx" : idx,
+    #                     "vec": ind["vec"],
+    #                     "fitness": ind["fitness"],
+    #                  }
+    #             )
+
+    #     for i in range(len(self.crossover_vecs), self.pop_size):
+    #         vec = [random.randint(0, 1) for _ in range(self.n_genes)]
+
+    #         self.pop.append({
+    #             "idx": i,
+    #             "vec" : vec,
+    #             "fitness" : self.score_func(vec)
+    #         })
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,56 +1,103 @@
-import random
+# import random
 from src.generation import Generation
+from numpy import dot
+from numpy.linalg import norm
 
-pop_size = 5
-vec_len = 5
-
-generation_no_crossover = Generation(pop_size, vec_len,
-                                     lambda x: sum(x), [])
-generation_3_crossover = Generation(pop_size, vec_len,
-                                    lambda x: sum(x), [
-    {
-        "ind":0,
-        "vec": [random.randint(0, 1) for _ in range(vec_len)],
-        "fitness":1,
-    },
-    {
-        "ind":1,
-        "vec": [random.randint(0, 1) for _ in range(vec_len)],
-        "fitness": 1,
-    },
-    {
-        "ind":2,
-        "vec": [random.randint(0, 1) for _ in range(vec_len)],
-        "fitness": 1}
-])
+#### FIXTURE #####
 
 
-def test_vec_len():
-    pop_1 = generation_no_crossover.pop
+def scoring_func(vec):
+    soln = [0, 1, 0, 1, 0]
+    cos_sim = dot(soln, vec) / (norm(soln) * norm(vec))
+    return cos_sim
+
+pop_size = 8
+n_genes = 5
+
+#### WITHOUT INITAL POP / CROSSOVER VECS ####
+g1 = Generation(
+    pop_size, n_genes, scoring_func
+)
+
+#### WITH INITAL POP / CROSSOVER VECS ####
+crossover_vecs = [
+    [1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1],
+    [0, 1, 0, 1, 0]
+]
+
+pop_size2 = 3
+n_genes2 = 5
+
+g2 = Generation(
+    pop_size2, n_genes2, scoring_func, crossover_vecs=crossover_vecs
+)
+#####################
+
+
+def test_n_genes():
+    pop1 = g1.pop
     for v in pop_1:
-        assert len(v["vec"]) == vec_len
-
-    pop_2 = generation_3_crossover.pop
-    for v in pop_2:
-        assert len(v["vec"]) == vec_len
+        assert len(v["vec"]) == n_genes
 
 
-def test_vec_vals():
-    pop_1 = generation_no_crossover.pop
-    for v in pop_1:
-        assert sum(v["vec"]) <= vec_len
+def test_n_genes():
+    pop1 = g1.pop
+    for v in pop1:
+        assert len(v.vec) == n_genes
 
-    pop_2 = generation_3_crossover.pop
-    for v in pop_2:
-        assert sum(v["vec"]) <= vec_len
+def test_sort_generation():
+    g2._sort_generation()
+    assert g2.pop[0] == [0, 1, 0, 1, 0]
+    assert g2.pop[1] == [1, 1, 1, 1, 1]
+    assert g2.pop[2] == [1, 0, 1, 0, 1]
 
 
-def test_pop_idx():
-    pop_1 = generation_no_crossover.pop
-    sum_of_idx = 0
 
-    for v in pop_1:
-        sum_of_idx += v["idx"]
 
-    assert sum_of_idx == (pop_size*4)/2
+# generation_no_crossover = Generation(pop_size, n_genes,
+#                                      lambda x: sum(x), [])
+# generation_3_crossover = Generation(pop_size, n_genes,
+#                                     lambda x: sum(x), [
+#     {
+#         "ind":0,
+#         "vec": [random.randint(0, 1) for _ in range(n_genes)],
+#         "fitness":1,
+#     },
+#     {
+#         "ind":1,
+#         "vec": [random.randint(0, 1) for _ in range(n_genes)],
+#         "fitness": 1,
+#     },
+#     {
+#         "ind":2,
+#         "vec": [random.randint(0, 1) for _ in range(n_genes)],
+#         "fitness": 1}
+# ])
 
+
+
+
+#     pop_2 = generation_3_crossover.pop
+#     for v in pop_2:
+#         assert len(v["vec"]) == n_genes
+
+
+# def test_vec_vals():
+#     pop_1 = generation_no_crossover.pop
+#     for v in pop_1:
+#         assert sum(v["vec"]) <= n_genes
+
+#     pop_2 = generation_3_crossover.pop
+#     for v in pop_2:
+#         assert sum(v["vec"]) <= n_genes
+
+
+# def test_pop_idx():
+#     pop_1 = generation_no_crossover.pop
+#     sum_of_idx = 0
+
+#     for v in pop_1:
+#         sum_of_idx += v["idx"]
+
+#     assert sum_of_idx == (pop_size*4)/2
