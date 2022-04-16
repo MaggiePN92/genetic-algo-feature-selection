4b83eb80215d98fc6de79257decf3dc36107e3af
Magnus Nytun <Magnus@Magnuss-Air-4.lan>
2022-04-16 13:25:54 +0200

    algorithm runs

--- src/generation.py
+++ src/generation.py
@@ -1,24 +1,27 @@
-import random
-from src.individual import Individual
+from random import randint
+from individual import Individual
 
 
 class Generation:
-    def __init__(self, pop_size, n_genes, score_func, crossover_vecs = None):
+    def __init__(self, pop_size, n_genes, score_func,
+     crossover_vecs = [], mutation_prob = 0.1):
         self.pop_size = pop_size
         self.n_genes = n_genes
         self.score_func = score_func
         self.pop = []
-
-        if not crossover_vecs:
-            self._init_pop()
-        
-        else:
-            self.pop = crossover_vecs
-
-
+        self.mutation_prob = mutation_prob
+        self.crossover_vecs = crossover_vecs
+        self._init_pop()
+    
     def _init_pop(self):
-        for i in range(self.pop_size):
-            vec = [random.randint(0, 1) for _ in range(self.n_genes)]
+        if len(self.crossover_vecs) > 0:
+            for idx, vec in enumerate(self.crossover_vecs):
+                fitness = self.score_func(vec)
+                indi = Individual(idx, vec, fitness)
+                self.pop.append(indi)
+
+        for i in range(len(self.crossover_vecs), self.pop_size):
+            vec = [randint(0, 1) for _ in range(self.n_genes)]
             fitness = self.score_func(vec)
             
             indi = Individual(i, vec, fitness)
@@ -27,32 +30,56 @@ class Generation:
     def _sort_generation(self):
         self.pop.sort(key = lambda x: x.fitness)
 
-    def mutate(self):
-        raise NotImplemented
+    def _mutate(self):
+        mutation_prob_percent = self.mutation_prob * 100
+
+        for individual in self.pop:
+            print(individual.vec)
+            for idx, gene in enumerate(individual.vec):
+                if randint(1,100) <= mutation_prob_percent:
+                    individual.vec[idx] = self._flip_gene(gene)
+                    
+    def _flip_gene(self, gene: int) -> int:
+        assert ((gene == 1) or (gene == 0))
+
+        if gene == 1:
+            return 0
+        return 1
+
+    def _keep_n_fittest(self, n):
+        self.pop = self.pop[-n:]
+
+    def get_crossover_vecs(self):
+        crossover_vecs = []
+        for ind in self.pop:
+            crossover_vecs.append(ind.vec)
+        return crossover_vecs
+
+    def get_best():
+        return self.pop[-1]
 
 
+if __name__ == "__main__":
+    from numpy import dot
+    from numpy.linalg import norm  
 
-    # def _init_pop(self):
-    #     """
-    #     Lager en ny populasjon med individer ved en ny generasjon. Tar hensyn til crossover individer.
+    def scoring_func(vec):
+        soln = [0, 1, 0, 1, 0]
+        cos_sim = dot(soln, vec) / (norm(soln) * norm(vec))
+        return cos_sim
 
-    #     :return:
-    #     """
-    #     if len(self.crossover_vecs) > 0:
-    #         for idx, ind in enumerate(self.crossover_vecs):
-    #             self.pop.append(
-    #                 {
-    #                     "idx" : idx,
-    #                     "vec": ind["vec"],
-    #                     "fitness": ind["fitness"],
-    #                  }
-    #             )
+    crossover_vecs = [
+        [1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1],
+        [0, 1, 0, 1, 0]
+    ]
 
-    #     for i in range(len(self.crossover_vecs), self.pop_size):
-    #         vec = [random.randint(0, 1) for _ in range(self.n_genes)]
+    pop_size2 = 5
+    n_genes2 = 5
 
-    #         self.pop.append({
-    #             "idx": i,
-    #             "vec" : vec,
-    #             "fitness" : self.score_func(vec)
-    #         })
+    g2 = Generation(
+        pop_size2, n_genes2, scoring_func, crossover_vecs=crossover_vecs
+    )
+
+    g2._sort_generation()
+    print(g2.pop[-1].fitness)
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,4 +1,5 @@
 # import random
+from tkinter import S
 from src.generation import Generation
 from numpy import dot
 from numpy.linalg import norm
@@ -37,67 +38,23 @@ g2 = Generation(
 
 def test_n_genes():
     pop1 = g1.pop
-    for v in pop_1:
-        assert len(v["vec"]) == n_genes
-
-
-def test_n_genes():
-    pop1 = g1.pop
     for v in pop1:
         assert len(v.vec) == n_genes
 
 def test_sort_generation():
     g2._sort_generation()
-    assert g2.pop[0] == [0, 1, 0, 1, 0]
-    assert g2.pop[1] == [1, 1, 1, 1, 1]
-    assert g2.pop[2] == [1, 0, 1, 0, 1]
+    assert g2.pop[-1].vec == [0, 1, 0, 1, 0]
 
+def test_keep_n_fittest():
+    g3 = Generation(
+        8, 5, scoring_func
+    )
 
+    g3._keep_n_fittest(n = 6)
+    assert len(g3.pop) == 6
+    g3._keep_n_fittest(n = 4)
+    assert len(g3.pop) == 4
 
-
-# generation_no_crossover = Generation(pop_size, n_genes,
-#                                      lambda x: sum(x), [])
-# generation_3_crossover = Generation(pop_size, n_genes,
-#                                     lambda x: sum(x), [
-#     {
-#         "ind":0,
-#         "vec": [random.randint(0, 1) for _ in range(n_genes)],
-#         "fitness":1,
-#     },
-#     {
-#         "ind":1,
-#         "vec": [random.randint(0, 1) for _ in range(n_genes)],
-#         "fitness": 1,
-#     },
-#     {
-#         "ind":2,
-#         "vec": [random.randint(0, 1) for _ in range(n_genes)],
-#         "fitness": 1}
-# ])
-
-
-
-
-#     pop_2 = generation_3_crossover.pop
-#     for v in pop_2:
-#         assert len(v["vec"]) == n_genes
-
-
-# def test_vec_vals():
-#     pop_1 = generation_no_crossover.pop
-#     for v in pop_1:
-#         assert sum(v["vec"]) <= n_genes
-
-#     pop_2 = generation_3_crossover.pop
-#     for v in pop_2:
-#         assert sum(v["vec"]) <= n_genes
-
-
-# def test_pop_idx():
-#     pop_1 = generation_no_crossover.pop
-#     sum_of_idx = 0
-
-#     for v in pop_1:
-#         sum_of_idx += v["idx"]
-
-#     assert sum_of_idx == (pop_size*4)/2
+def test_mutation():
+    pass 
+    
